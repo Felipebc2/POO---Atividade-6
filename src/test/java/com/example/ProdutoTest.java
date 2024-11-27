ffe20f3441bcda7dce3975e8429eed81643eef8d
Felipe B. Castro <felipeb.castro02@gmail.com>
2024-11-26 22:53:10 -0300

    Update ProdutoTest.java

--- src/test/java/com/example/ProdutoTest.java
+++ src/test/java/com/example/ProdutoTest.java
@@ -1,4 +1,31 @@
 package com.example;
 
-public class ProdutoTest {
+import org.example.Produto;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdutoTest {
+
+    @Test
+    void testCriacaoProduto() {
+        Produto produto = new Produto("Produto1", 10);
+
+        assertEquals("Produto1", produto.getNome(), "O nome do produto deveria ser 'Produto1'");
+        assertEquals(10, produto.getQuantidade(), "A quantidade do produto deveria ser 10");
+    }
+
+    @Test
+    void testAtualizarQuantidade() {
+        Produto produto = new Produto("Produto1", 10);
+        produto.setQuantidade(20);
+
+        assertEquals(20, produto.getQuantidade(), "A quantidade do produto deveria ser atualizada para 20");
+    }
+
+    @Test
+    void testProdutoToString() {
+        Produto produto = new Produto("Produto1", 10);
+
+        assertEquals("Produto1 - 10 unidades", produto.toString(), "O método toString deveria retornar 'Produto1 - 10 unidades'");
+    }
 }
